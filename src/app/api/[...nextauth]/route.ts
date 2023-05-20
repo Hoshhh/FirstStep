8a3e64fd9b87ae16260b3ffcb9bd1949aad4eca9
Hoshhh <nyxious123@live.com>
2023-05-20 19:19:01 -0400

    next-auth

--- src/app/api/[...nextauth]/route.ts
+++ src/app/api/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ import { NextAuthOptions } from "next-auth"
 export const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
         })
     ]
 }
