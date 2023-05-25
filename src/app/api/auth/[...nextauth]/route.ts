0dea2fdd425649b38bb78246efe9b40537995d7e
Hoshhh <nyxious123@live.com>
2023-05-25 19:11:51 -0400

    setup basic auth with github and prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,25 @@
 import NextAuth from "next-auth"
+//import prisma from "../../../lib/prismadb"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GithubProvider from 'next-auth/providers/github'
 import { NextAuthOptions } from "next-auth"
+import { PrismaClient } from "@prisma/client"
 
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID as string,
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-        })
-    ]
+        }),
+    ],
+    secret: process.env.SECRET,
+    session: {
+        strategy: "jwt"
+    },
+    debug: process.env.NODE_ENV === "development"
 }
 
 const handler = NextAuth(authOptions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import Hero from '@/components/Hero'
 import Developers from '@/components/Developers'
+import User from '@/components/User'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  console.log(session)
   return (
     <div>
       <Hero />
       <Developers />
+      {JSON.stringify(session)}
+      <User />
     </div>
   )
 }
