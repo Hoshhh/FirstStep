c3d504b54d3c99eab31067548bc748624c927bc9
Hoshhh <nyxious123@live.com>
2023-05-26 19:59:50 -0400

    made the userid available in the session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import NextAuth from "next-auth"
-//import prisma from "../../../lib/prismadb"
+import prisma from "../../../lib/prismadb"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GithubProvider from 'next-auth/providers/github'
 import { NextAuthOptions } from "next-auth"
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -15,6 +15,22 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string
         }),
     ],
+    callbacks: {
+        jwt({ token, account, user }) {
+            if (account) {
+            token.accessToken = account.access_token
+            token.id = user?.id
+            }
+            return token
+        },
+        session({ session, token }) {
+            // I skipped the line below coz it gave me a TypeError
+            // session.accessToken = token.accessToken;
+            session.user.id = token.id;
+
+            return session;
+        },
+    },
     secret: process.env.SECRET,
     session: {
         strategy: "jwt"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,20 @@ import Developers from '@/components/Developers'
 import User from '@/components/User'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
+import prisma from '../app/lib/prismadb'
+import { NextApiRequest } from 'next'
 
-export default async function Home() {
+export default async function Home(req: NextApiRequest) {
   const session = await getServerSession(authOptions)
+
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      id: session?.user.id
+    }
+  })
+
+  console.log(currentUser)
+
   console.log(session)
   return (
     <div>
