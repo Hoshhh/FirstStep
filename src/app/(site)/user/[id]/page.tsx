8ce9f93909db97794d0cc06a64be81240931e9bc
Hoshhh <nyxious123@live.com>
2023-06-07 23:10:24 -0400

    Started profile page styling

--- src/app/(site)/user/[id]/page.tsx
+++ src/app/(site)/user/[id]/page.tsx
@@ -13,19 +13,18 @@ export default async function page({ params }: {
     console.log(currentUser?.firstName)
 
   return (
-    <div className="pt-32">
-      <p className='py-4'>
-        User: {currentUser?.id}
-      </p>
-      <p className='py-4'>
-        Name: {currentUser?.name}
-      </p>
-      <p className='py-4'>
-        Created At: {currentUser?.createdAt.toString()}
-      </p>
-      <p className='py-4'>
-        First Name: {currentUser?.position}
-      </p>
+    <div className="grid grid-cols-4 gap-4 h-screen">
+      <div className='flex flex-col col-span-1 border-r-2 items-center'>
+        <h2 className='text-2xl pt-8'>FirstStepTech</h2>
+        <div className='flex flex-col items-center pt-20'>
+          <img src={currentUser?.image!} alt="Profile Picture" className="w-24 h-24 rounded-full shadow-md shadow-gray-400" />
+          <h4 className='pt-2 font-normal text-slate-800 text-md'>Joshua Johnson</h4>
+          <h4 className='pt-1 font-normal text-slate-400 text-xs'>Full Stack Web Developer</h4>
+        </div>
+      </div>
+      <div className='flex col-span-3 justify-center items-center'>
+        Test Col 2
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route'
 import prisma from '../app/lib/prismadb'
 import { NextApiRequest } from 'next'
+import Navbar from '@/components/Navbar'
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -22,6 +23,7 @@ export default async function Home() {
   console.log(session)
   return (
     <div>
+      <Navbar />
       <Hero />
       <Developers />
       <User />
