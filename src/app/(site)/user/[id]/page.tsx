3c2a1fd802400c5aa7c3a44fd12243021a4ecc4b
Hoshhh <nyxious123@live.com>
2023-06-09 18:36:21 -0400

    more profile styling

--- src/app/(site)/user/[id]/page.tsx
+++ src/app/(site)/user/[id]/page.tsx
@@ -13,13 +13,25 @@ export default async function page({ params }: {
     console.log(currentUser?.firstName)
 
   return (
-    <div className="grid grid-cols-4 gap-4 h-screen">
+    <div className="md:grid md:grid-cols-4 md:gap-4 md:h-screen">
       <div className='flex flex-col col-span-1 border-r-2 items-center'>
         <h2 className='text-2xl pt-8'>FirstStepTech</h2>
-        <div className='flex flex-col items-center pt-20'>
+        <div className='flex flex-col items-center pt-16 p-4 border-b-2'>
           <img src={currentUser?.image!} alt="Profile Picture" className="w-24 h-24 rounded-full shadow-md shadow-gray-400" />
           <h4 className='pt-2 font-normal text-slate-800 text-md'>Joshua Johnson</h4>
           <h4 className='pt-1 font-normal text-slate-400 text-xs'>Full Stack Web Developer</h4>
+        </div>
+        <div className='pt-12 h-full'>
+          <ul className='grid grid-rows-5 content-between h-full'>
+            <li>About</li>
+            <li>Technical Skills</li>
+            <li>Links</li>
+            <li>Availability</li>
+            <li>CV/Resume</li>
+          </ul>
+        </div>
+        <div className='mb-12 mt-8'>
+          Sign Out
         </div>
       </div>
       <div className='flex col-span-3 justify-center items-center'>
