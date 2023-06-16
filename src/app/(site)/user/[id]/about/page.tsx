137f046a75741fd9d396ee4f9d63c72f48ddbf92
Hoshhh <nyxious123@live.com>
2023-06-16 19:10:51 -0400

    about page  styling

--- src/app/(site)/user/[id]/about/page.tsx
+++ src/app/(site)/user/[id]/about/page.tsx
@@ -7,6 +7,20 @@ export default async function Page({ params }: {
     const currentUser = await data.json()
     console.log(currentUser)
   return (
-    <div>{currentUser.email}</div>
+    <div className='flex flex-col w-3/4 items-center'>
+      <h2 className='tracking-widest pb-12'>About Me</h2>
+      <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex flex-col justify-center p-2 hover:scale-105 ease-in duration-300">
+        <div className='flex w-full p-4 border-b-2 items-center'>
+          <img src={currentUser.image} alt="Profile Picture" className="w-24 h-24 rounded-full shadow-md shadow-gray-400 mr-2" />
+          <div className='flex flex-col'>
+            <h4 className='pt-2 font-normal text-slate-800 text-md'>Joshua Johnson</h4>
+            <h4 className='pt-1 font-normal text-slate-400 text-xs'>Full Stack Web Developer</h4>
+          </div>
+        </div>
+        <p className='text-sm m-4'>
+          All about me!
+        </p>
+      </div>
+    </div>
   )
 }
